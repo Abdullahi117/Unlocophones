8cda1f330ff3e6cf35268d4797f0bd0b49bd454e
mhussein29 <77996250+mhussein29@users.noreply.github.com>
2021-04-08 17:38:27 -0500

    edit account

---
+++ account.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "header.php"; ?>
+
+
+<body>
+    <div class="logo"><a href="index.html"><img src="Unloco.png" alt=""></a>
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="contact.html">Contact</a></li>
+                 
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="google.html">Google</a></li>
+                    <li><a href="apple.html">Apple</a></li>
+                    <li><a href="samsung.html">Samsung</a></li>
+                    <li><a href="index.html">Home</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <section class= "editbox">
+        <header class="accountdetails">
+            <h1>Edit Account Details</h1>
+        </header>
+        <form class="editform" action="account.php" method="POST">
+            <label for="firstname">First Name: </label>
+            <input type="text" name="firstname" id="firstname" placeholder="First Name...">
+            <label for="lastname">Last Name: </label>
+            <input type="text" name="lastname" id="lastname" placeholder="Last Name...">
+            <label for="email">Email Address: </label>
+            <input type="text" name="email" id="email" placeholder="Email...">
+            <label for="password">Password: </label>
+            <input type="text" name="password" id="password" placeholder="Password...">
+            <label for="confirmpass">Confirm Password: </label>
+            <input type="text" name="confirmpass" id="confirmpass" placeholder="Confirm Password...">
+            <input type="submit" value="Edit">
+        </form>
+        
+    </section>
+   
+
+    <?php include "footer.php"; ?>
+
+</body>
+</html>
