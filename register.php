a12ab3540251446389789fccc6ed042e611f90e9
mhussein29 <77996250+mhussein29@users.noreply.github.com>
2021-04-11 20:00:14 -0500

    .

--- register.php
+++ register.php
@@ -1,96 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php 
-$firstName = $lastName = $username = $email = $password1 = $password2 = "";
-$firstNameErr = $lastNameErr = $usernameErr = $emailErr = $password1Err = $password2Err = "";
+session_start();
+$firstName = $lastName = $address = $phoneNumber = $username = $email = $password1 = $password2 = "";
+$firstNameErr = $lastNameErr = $addressErr = $phoneNumberErr= $usernameErr = $emailErr = $password1Err = $password2Err = "";
 
 //establish connection with database
-$mysqli = new mysqli('localhost', 'root', '', 'unloco');
+$mysqli = new mysqli('localhost', 'root', '', 'mydb');
 if($mysqli->connect_errno) {
     //error if connection fails
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $test = true;
     $firstName = $mysqli->real_escape_string($_POST['firstName']);
     $lastName = $mysqli->real_escape_string($_POST['lastName']);
     $username = $mysqli->real_escape_string($_POST['username']);
     $email = $mysqli->real_escape_string($_POST['email']);
     $password = ($_POST['username']);
+
     //First Name checker 
     if (empty($_POST["firstName"])) {
         $firstNameErr = "Name is required";
-        exit;
+        $test = false;
     } 
     else {
         $firstName = test_input($_POST["firstName"]);
         if (!preg_match("/^[a-zA-Z]*$/",$firstName)) {
             $firstNameErr = "Only letters are allowed";
-            exit;
         }
     }
     //Last Name checker 
     if (empty($_POST["lastName"])) {
         $lastNameErr = "Name is required";
-        exit;
     } 
     else {
         $lastName = test_input($_POST["lastName"]);
         // check if name only contains letters and whitespace
         if (!preg_match("/^[a-zA-Z-' ]*$/",$lastName)) {
             $lastNameErr = "Only letters and white space allowed";
-            exit;
-            }
+        }
     }
+    //Address Checker
+    if (empty($_POST["address"])) {
+        $addressErr = "Address Required";
 
+    }
+    else {
+        $address = test_input($_POST["address"]);
+        //address validation
+        if (!preg_match("/^[a-zA-Z0-9-\/] ?([a-zA-Z0-9-\/]|[a-zA-Z0-9-\/] )*[a-zA-Z0-9-\/]$/", $address)) {
+            $addressErr = "Must Be A Proper Address";
+        }
+    }
+    //check phonenumber
+    if (empty($_POST["phoneNumber"])) {
+        $phoneNumberErr = "Phone Number Required";
+
+    }
+    else {
+        $phoneNumber = test_input($_POST["address"]);
+        //phone number validation
+        if (!preg_match("/^\d{10}$/", $phoneNumber)) {
+            $phoneNumberErr = "Please Input 10-digit Phone Number";
+
+        }
+    }
     //Password1 checker 
     if (empty($_POST["password1"])) {
         $password1Err = "Password is required";
-        exit;
     } 
     else {
         $password1 = test_input($_POST["password1"]);
         // check if name only contains letters and whitespace
         if (!preg_match("/[0-9A-Za-z!@#$%]/",$password1)) {
             $password1Err = "Only letters, numbers and special characters allowed";
-            exit;
         }
     }    
 
     //Password2 confirm checker 
     if ($password1 != $password2) {
         $password2Err = "Passwords dont match";
-        exit;
-
     }
           
 
     //Username checker 
     if (empty($_POST["username"])) {
         $usernameErr = "Userame is required";
-        exit;
     } 
     else {
         $username = test_input($_POST["username"]);
         if (!preg_match("/^[a-zA-Z0-9]/",$username)) {
             $usernameErr = "Only letters and numbers allowed";
-            exit;
         }
     }
     
     //Email checker 
     if (empty($_POST["email"])) {
         $emailErr = "Email is required";
-        exit;
     } 
     else {
         $email = test_input($_POST["email"]);
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $emailErr = "Invalid email format";
-            exit;
         }
     }
-
     //insert sql query
     $sql = "INSERT INTO customer (first_name, last_name, email) VALUES "
                 . " ('$firstName', '$lastName', '$email')";
@@ -102,7 +117,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $_SESSION['message'] = "User could not be added to the database";
     }
-
 }
 
 function test_input($data) {
@@ -123,37 +137,49 @@ function test_input($data) {
                 <p>Please fill out the information below to create an account.</p>
                     <div>
                         <label>First Name</label>
-                        <input type="text" name="firstName" placeholder="First Name">
+                        <input type="text" name="firstName" placeholder="First Name" required>
                         <br>
                         <span class ="error"><?php echo $firstNameErr;?></span>
                     </div>
                     <div>
                         <label>Last Name</label>
-                        <input type="text" name="lastName" placeholder="Last Name" >
+                        <input type="text" name="lastName" placeholder="Last Name" required>
                         <br>
                         <span class = "error"><?php echo $lastNameErr;?></span>
                     </div>
                     <div>
+                        <label>Address</label>
+                        <input type="text" name="address" placeholder="Address" required>
+                        <br>
+                        <span class = "error"><?php echo $addressErr;?></span>
+                    </div>
+                    <div>
+                        <label>Phone Number</label>
+                        <input type="text" name="phoneNumber" placeholder="Phone Number" required>
+                        <br>
+                        <span class = "error"><?php echo $phoneNumberErr;?></span>
+                    </div>
+                    <div>
                         <label>Username</label>
-                        <input type="text" name="username" placeholder="Username" >
+                        <input type="text" name="username" placeholder="Username" required>
                         <br>
                         <span class = "error"><?php echo $usernameErr;?></span>
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" name="password1" placeholder="Password" >
+                        <input type="password" name="password1" placeholder="Password" required>
                         <br>
                         <span class = "error"><?php echo $password1Err;?></span>
                     </div>
                     <div>
                         <label>Confirm password</label>
-                        <input type="password" name="password2" placeholder="Confirm Password" >
+                        <input type="password" name="password2" placeholder="Confirm Password" required>
                         <br>
                         <span class = "error"><?php echo $password2Err;?></span>
                     </div>
                     <div>
                         <label>Email</label>
-                        <input type="text" name="email" placeholder="Email" >
+                        <input type="text" name="email" placeholder="Email" required>
                         <br>
                         <span class = "error"><?php echo $emailErr;?></span>
                     </div>
