cc7379b53c4c2da63bf1a28033585be945716616
mhussein29 <77996250+mhussein29@users.noreply.github.com>
2021-04-12 18:56:27 -0500

    .

--- account.php
+++ account.php
@@ -2,7 +2,6 @@
 <html lang="en">
 
 <?php
-include "header.php";
 session_start();
 $firstName = $lastName = $address = $phoneNumber = $username = $email = $password1 = $password2 = "";
 $firstNameErr = $lastNameErr = $addressErr = $phoneNumberErr= $usernameErr = $emailErr = $password1Err = $password2Err = "";
@@ -21,7 +20,21 @@ if($mysqli->connect_errno) {
     $username = $mysqli->real_escape_string($_POST['username']);
     $email = $mysqli->real_escape_string($_POST['email']);
     $password = ($_POST['username']);
-                ?>
+   }
+
+   //Udpate Query 
+   $sql = "UPDATE customer " .
+   "SET first_name = '$firstName', last_name = '$lastName'" . 
+   " WHERE customer_id = 4";
+
+   if ($mysqli->query($sql) === true) {
+    $_SESSION['message'] = "Update Complete!";
+    } else {
+    $_SESSION['message'] = "Could not connect to the database";
+}             
+?>
+
+<?php include "header.php"; ?>
 
     
     
@@ -31,38 +44,20 @@ if($mysqli->connect_errno) {
             <h1>Edit Account Details</h1>
         </header>
         <form class="editform" action="account.php" method="POST">
-            <label for="firstname">First Name: </label>
-            <input type="text" name="firstname" id="firstname" placeholder="First Name...">
-            <label for="lastname">Last Name: </label>
-            <input type="text" name="lastname" id="lastname" placeholder="Last Name...">
+            <label for="firstName">First Name: </label>
+            <input type="text" name="firstName" id="firstName" placeholder="First Name...">
+            <label for="lastName">Last Name: </label>
+            <input type="text" name="lastName" id="lastName" placeholder="Last Name...">
             <label for="email">Email Address: </label>
             <input type="text" name="email" id="email" placeholder="Email...">
-            <label for="password">Password: </label>
-            <input type="text" name="password" id="password" placeholder="Password...">
-            <label for="confirmpass">Confirm Password: </label>
-            <input type="text" name="confirmpass" id="confirmpass" placeholder="Confirm Password...">
             <input type="submit" value="Edit">
         </form>
-        
     </section>
 
 
     <button><a href="orderhistory.php">Order History</a></button>
-    
-    
-   <?php
-       
-                          //Udpate Querey 
-$sql =UPDATE customers
-SET first_name = $fistName, last_name=$lastName
-WHERE customer_id =3;
-      $result = mysqli_query($mysqli, $sql);
-       echo "$result";
 
-       
-       
-       ?>
-    <?php include "footer.php"; ?>
+
+     <?php include "footer.php"; ?>
 
 </body>
-</html>
